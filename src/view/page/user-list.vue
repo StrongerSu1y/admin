1ab22b5efa5166f1e5942c4cced23973fca278e8
junaihui <wangcj520xlh@gmail.com>
2018-07-31 19:39:38 +0800

    初始版本

---
+++ src/view/page/user-list.vue
@@ -0,0 +1,108 @@
+<style lang="less">
+    @import '../../style/common.less';
+    @import './user/table.less';
+</style>
+
+<template>
+    <div style="height: 100%">
+        <Row :gutter="10">
+            <Col span="24">
+                <Card>
+                    <p slot="title">
+                        <Icon type="help-buoy"></Icon>
+                        请输入查询条件
+                    </p>
+                    <Row>
+                        <Input v-model="searchConName" @on-change="handleSearch2" icon="search" placeholder="请输入姓名..."
+                               style="width: 200px"/>
+                        <Input v-model="searchConTel" @on-change="handleSearch2" icon="search" placeholder="请输入手机号..."
+                               style="width: 200px"/>
+                    </Row>
+                    <Row class="margin-top-10 searchable-table-con1">
+                        <Table :columns="columns1" :data="pageUserList"></Table>
+                    </Row>
+                </Card>
+            </Col>
+        </Row>
+        <div class="page-style">
+            <Page :total="pageTotal" :current="pageNum" :page-size="pageSize" show-elevator show-total placement="top"
+                  @on-change="changePage"></Page>
+
+        </div>
+    </div>
+</template>
+
+<script>
+import * as table from './user/search'
+import axios from 'axios'
+
+export default {
+  name: 'searchable-table',
+  data () {
+    return {
+      searchConName: '',
+      searchConTel: '',
+      columns1: table.columns1,
+      userList: [],
+      pageUserList: [],
+      initTable: [],
+      pageTotal: 0,
+      pageNum: 1,
+      pageSize: 15
+    }
+  },
+  methods: {
+    init () {
+      var self = this
+      var url = 'http://10.0.0.24:8100/admin/user/list'
+      axios({
+        method: 'get',
+        url: url,
+        mode: 'no-cors',
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjcmVhdGUiOjE1MzI2NTg4OTY1MDIsImV4cCI6MTUzMzI2MzY5NiwidXNlcm5hbWUiOiJ3YW5nY2oifQ.7c8ofidy76Y-PmUd2np0PYnE7amwem4fj_x-DZrFq6U'
+        },
+        withCredentials: true
+      }).then(function (response) {
+        console.log('用户', response.data.data.data)
+        self.userList = self.initTable = response.data.data.data
+        self.pageTotal = self.userList.length
+        self.pageUserList = self.userList.slice(0, self.pageSize)
+      }).catch(function (error) {
+        console.log(error)
+      })
+    },
+
+    changePage (index) {
+      var _start = (index - 1) * this.pageSize
+      var _end = index * this.pageSize
+      this.pageUserList = this.userList.slice(_start, _end)
+    },
+    search (data, argumentObj) {
+      let res = data
+      let dataClone = data
+      for (let argu in argumentObj) {
+        if (argumentObj[argu].length > 0) {
+          res = dataClone.filter(d => {
+            return d[argu].indexOf(argumentObj[argu]) > -1
+          })
+          dataClone = res
+        }
+      }
+      return res
+    },
+    handleSearch2 () {
+      this.userList = this.initTable
+      this.userList = this.search(this.userList, {username: this.searchConName, tel: this.searchConTel})
+      this.pageTotal = this.userList.length
+      this.pageUserList = this.userList.slice(0, this.pageSize)
+    }
+
+  },
+  mounted () {
+    this.init()
+  }
+}
+</script>
---
+++ src/view/page/user/search.js
@@ -0,0 +1,87 @@
+export const columns1 = [
+    {
+        key: 'username',
+        title: '用户名'
+    },
+    {
+        key: 'tel',
+        title: '电话号码'
+    },
+    {
+        key: 'password',
+        title: '密码'
+    },
+    {
+        key: 'nickname',
+        title: '昵称'
+    },
+    {
+        key: 'createdDate',
+        title: '创建时间'
+    },
+    {
+        key: 'avatar',
+        title: '头像'
+    },
+    {
+        key: 'salt',
+        title: '电话号码'
+    }
+];
+
+export const searchTable1 = [
+    {
+        name: 'Aresn',
+        tel: '17712345678'
+    },
+    {
+        name: 'Lison',
+        tel: '17787654321'
+    },
+    {
+        name: 'Lili',
+        tel: '12212345678'
+    },
+    {
+        name: 'Lucy',
+        tel: '13312345678'
+    }
+];
+
+export const searchTable2 = [
+    {
+        name: 'Aresn',
+        tel: '17712345678'
+    },
+    {
+        name: 'Lison',
+        tel: '17787654321'
+    },
+    {
+        name: 'Lili',
+        tel: '12212345678'
+    },
+    {
+        name: 'Lucy',
+        tel: '13312345678'
+    }
+];
+
+export const searchTable3 = [
+    {
+        name: 'Aresn',
+        tel: '17712345678'
+    },
+    {
+        name: 'Lison',
+        tel: '17787654321'
+    },
+    {
+        name: 'Lili',
+        tel: '12212345678'
+    },
+    {
+        name: 'Lucy',
+        tel: '13312345678'
+    }
+];
